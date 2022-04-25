474a9fe9edbb253fbe6f21386e1fbcc620ce4078
Tony Curtis <tcurtis45@yahoo.com>
2022-04-25 15:04:44 -0500

    Added lab03

---
+++ Labs/lab03/final/helpers.py
@@ -0,0 +1,31 @@
+import tabulate
+import time
+
+def print_route_list(route_list):
+
+    headers = [
+        'Host-Name',
+        'System IP',
+        'Destination Route',
+        'Routing Instance',
+        'Route Metric',
+    ]
+
+    table = []
+
+    for route in route_list:
+            row = [
+                route['vdevice-host-name'],
+                route['vdevice-name'],
+                route['route-destination-prefix'],
+                route['routing-instance-name'],
+                route['route-metric'],
+            ]
+            table.append(row)
+
+    try:
+        print(tabulate.tabulate(table, headers, tablefmt="fancy_grid"))
+        time.sleep(1)
+    except UnicodeEncodeError:
+        print(tabulate.tabulate(table, headers, tablefmt="grid"))
+        time.sleep(1)
